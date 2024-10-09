66697dfe60a51ad93ee3611a20e390f908f6537f
syun560 <bajikoji@gmail.com>
2024-10-09 21:20:26 +0900

    update

--- next.config.js
+++ next.config.js
@@ -11,20 +11,7 @@ const nextConfig = {
                 hostname: 'localhost'
             }
         ]
-    },
-    // async headers() {
-    //     return [
-    //         {
-    //             "source": "/",
-    //             "headers": [
-    //                 { "key": "Access-Control-Allow-Credentials", "value": "true" },
-    //                 { "key": "Access-Control-Allow-Origin", "value": "http://localhost:50021" },
-    //                 { "key": "Access-Control-Allow-Methods", "value": "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-    //                 { "key": "Access-Control-Allow-Headers", "value": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
-    //             ]
-    //         }
-    //     ]
-    // },
+    }
 }
 
 module.exports = nextConfig
